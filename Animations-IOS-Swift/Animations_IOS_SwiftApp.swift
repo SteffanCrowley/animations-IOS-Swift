2d4058c2fe393e900988e126535422f03dce8fb3
Steffan Crowley <steffancrow@gmail.com>
2022-08-22 12:59:56 -0700

    Initial Commit

---
+++ Animations-IOS-Swift/Animations_IOS_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animations_IOS_SwiftApp.swift
+//  Animations-IOS-Swift
+//
+//  Created by steffan crowley on 8/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Animations_IOS_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animations-IOS-Swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animations-IOS-Swift
+//
+//  Created by steffan crowley on 8/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
